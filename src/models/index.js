e820f49563eada09561d9affa2ba60ee0802357a
Newton Lomar <newtonlomar16@gmail.com>
2021-04-01 08:46:12 -0300

    Express API with Games Endpoint

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const { connect, Mongoose } = require('mongoose');
+const MONGODB_URL = process.env.DATABASE;
+
+module.exports = () => {
+
+	const options = {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	};
+
+	return connect(MONGODB_URL, options);
+};
