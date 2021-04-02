491f2b5a9fc0a57b366a6f93dc76f122d92093e1
Newton Lomar <newtonlomar16@gmail.com>
2021-04-02 15:22:43 -0300

    Games Store, Update and Delete

--- src/models/Games.js
+++ src/models/Games.js
@@ -9,7 +9,7 @@ const GameSchema = new Schema({
 	otherTitles: [String],
 	developers: [String],
 	publishers: [String],
-	genrers: [String],
+	genres: [String],
 	firstReleased: Date,
 	japanRelease: Date,
 	usaRelease: Date,
@@ -56,4 +56,15 @@ module.exports = {
 
 		return query.exec();
 	},
+	store: (data) => {
+		const game = new Game(data);
+
+		return game.save();
+	},
+	update: (id, data, options = { new: true }) => {
+		return Game.findOneAndUpdate({_id: id}, data, options);
+	},
+	destroy: (id) => {
+		return Game.deleteOne({ _id: id });
+	}
 };
